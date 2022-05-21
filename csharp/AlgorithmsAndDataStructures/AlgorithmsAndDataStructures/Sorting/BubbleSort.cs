240b6845a74934333163a2a57207a69b9087fdad
Mathias Storm <mathias@storm.lc>
2022-05-21 09:21:52 +0200

    feat: add bubblesort

---
+++ csharp/AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/Sorting/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmsAndDataStructures.Sorting;
+
+public class BubbleSort : ISorter
+{
+    public void Sort(int[] arr)
+    {
+
+        while (true)
+        {
+            var swapped = false;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i + 1 < arr.Length && arr[i] > arr[i + 1])
+                {
+                    swapped = true;
+                    (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+                }
+            }
+
+            if (!swapped)
+            {
+                break;
+            }
+        }
+    }
+}
