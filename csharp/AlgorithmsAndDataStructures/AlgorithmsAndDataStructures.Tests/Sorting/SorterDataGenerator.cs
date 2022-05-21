362f458fe2db21826d4272d065e9f49aaf90b822
Mathias Storm <mathias@storm.lc>
2022-05-21 09:18:42 +0200

    fix: bug due to array state from dataclass leaking into other algo tests

--- csharp/AlgorithmsAndDataStructures/AlgorithmsAndDataStructures.Tests/Sorting/SorterDataGenerator.cs
+++ csharp/AlgorithmsAndDataStructures/AlgorithmsAndDataStructures.Tests/Sorting/SorterDataGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using AlgorithmsAndDataStructures.Sorting;
 
@@ -6,9 +7,10 @@ namespace AlgorithmsAndDataStructures.Tests.Sorting;
 
 public class SorterDataGenerator : IEnumerable<object[]>
 {
-    private List<ISorter> _algos = new List<ISorter>
+    private List<ISorter> _algos = new()
     {
-        new SelectionSort()
+        new SelectionSort(),
+        new BubbleSort()
     };
 
     private List<(int[], int[])> _testLoads = new()
@@ -18,6 +20,19 @@ public class SorterDataGenerator : IEnumerable<object[]>
         (new[] { 5, 2, 3, 1, 4, 6, 9, 8, 7 }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
     };
 
+    private void CopyArray(int[] input, int[] output)
+    {
+        if (output.Length != input.Length)
+        {
+            throw new ArgumentException(nameof(output), $"Output array doesnt match expected length");
+        }
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            output[i] = input[i];
+        }
+    }
+    
     public IEnumerator<object[]> GetEnumerator()
     {
         foreach (var algo in _algos)
@@ -25,7 +40,13 @@ public class SorterDataGenerator : IEnumerable<object[]>
             foreach (var testLoad in _testLoads)
             {
                 var (input, expectedOutput) = testLoad;
-                yield return new object[] { algo, input, expectedOutput };
+                var cpyIn = new int[input.Length];
+                var cpyOut = new int[expectedOutput.Length];
+
+                CopyArray(input, cpyIn);
+                CopyArray(expectedOutput, cpyOut);
+                
+                yield return new object[] { algo, cpyIn, cpyOut };
             }
         }
     }
